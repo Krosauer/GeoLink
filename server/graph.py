61af6b4947e2b7748e5c850c7b5d52d0533a1743
Krosauer <kyle.rosauer@icloud.com>
2025-01-11 14:59:10 -0600

    test

---
+++ server/graph.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+
+nodes_df = pd.read_csv('../Data/countries_new.csv')
+edges_df = pd.read_csv('../Data/edges.csv')
+
+G = nx.MultiGraph()
+for _, row in nodes_df.iterrows():
+    G.add_node(row['name'], cca3=row['cca3'], cca2=row['cca2'], un_member=row['un_member'], 
+               currencies=row['currencies'], languages=row['languages'], 
+               landlocked=row['landlocked'], area=row['area'], population=row['population'], 
+               timezones=row['timezones'], flag_colors=row['flag_colors'],
+               population_density=row['population_density'], gdp_per_capita=row['GDP Per Capita'],
+               exports=row['exports'], imports=row['imports'])
+
+valid_nodes = set(G.nodes())
+for index, row in edges_df.iterrows():
+    country1 = row['source']
+    country2 = row['target']
+    if country1 in valid_nodes and country2 in valid_nodes:
+        G.add_edge(country1, country2, connection_type=row['type'])
+
+for node in G.nodes:
+    G.nodes[node]["name"] = node
+
--- server/main.py
+++ server/main.py
@@ -1,9 +1,16 @@
 from flask import Flask,jsonify
 from flask_cors import CORS
+import graph
+import networkx as nx
 
 app = Flask(__name__)
 cors = CORS(app, origins='*')
 
+@app.route('/graph', methods=['GET'])
+def getGraph():
+    graph_data = nx.node_link_data(graph.G)
+    print(jsonify(graph_data))
+    return jsonify(graph_data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
