6c16d89cf51d0caf40e9bab73c4bbd92b270e884
Krosauer <kyle.rosauer@icloud.com>
2025-01-11 19:46:42 -0600

    save working graph sending to front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,37 @@
-import React, { useState } from 'react'
-import GuessedState from './Components/CountryDisplays/GuessedState.jsx'
-import BaseState from './Components/CountryDisplays/BaseState.jsx'
-import PendingState from './Components/CountryDisplays/PendingState.jsx'
-import './index.css'
-import { FaCoins } from 'react-icons/fa'
-import { FaComment } from 'react-icons/fa'
-import { FaLock } from 'react-icons/fa'
-import {FaExpand} from 'react-icons/fa'
-import {FaPeopleArrows} from 'react-icons/fa'
-import { FaMoneyBill } from 'react-icons/fa'
-import { FaClock } from 'react-icons/fa'
-import { FaPaintBrush } from 'react-icons/fa'
-import { FaCity } from 'react-icons/fa'
-import { FaLink } from 'react-icons/fa'
-import { FaLayerGroup } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import GuessedState from "./Components/CountryDisplays/GuessedState.jsx";
+import BaseState from "./Components/CountryDisplays/BaseState.jsx";
+import PendingState from "./Components/CountryDisplays/PendingState.jsx";
+import "./index.css";
+import { FaCoins } from "react-icons/fa";
+import { FaComment } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { FaExpand } from "react-icons/fa";
+import { FaPeopleArrows } from "react-icons/fa";
+import { FaMoneyBill } from "react-icons/fa";
+import { FaClock } from "react-icons/fa";
+import { FaPaintBrush } from "react-icons/fa";
+import { FaCity } from "react-icons/fa";
+import { FaLink } from "react-icons/fa";
+import { FaLayerGroup } from "react-icons/fa";
+import axios from "axios";
 
 function App() {
-  return (
-    <>
-    </>
-  )
+  const [graph, setGraph] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/graph")
+      .then((response) => {
+        setGraph(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return <></>;
 }
 
-export default App
+export default App;
--- server/graph.py
+++ server/graph.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import networkx as nx
-import matplotlib.pyplot as plt
 
-nodes_df = pd.read_csv('../Data/countries.csv')
+nodes_df = pd.read_csv('../Data/new_countries.csv')
 edges_df = pd.read_csv('../Data/edges.csv')
 
 G = nx.MultiGraph()
 for _, row in nodes_df.iterrows():
-    G.add_node(row['name'], cca3=row['cca3'], un_member=row['un_member'], 
+    G.add_node(row['name'], cca3=row['cca3'], cca2=row['cca2'], un_member=row['un_member'], 
                currencies=row['currencies'], languages=row['languages'], 
                landlocked=row['landlocked'], area=row['area'], population=row['population'], 
                timezones=row['timezones'], flag_colors=row['flag_colors'],
@@ -21,4 +20,4 @@ for index, row in edges_df.iterrows():
     if country1 in valid_nodes and country2 in valid_nodes:
         G.add_edge(country1, country2, connection_type=row['type'])
 
-print(G.number_of_edges())
+
